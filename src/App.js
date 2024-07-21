9cc326a8bcddd1c5b04bcbf0f01e3ea5ae04f69a
BinayakDeveloper <banjarajajabar@gmail.com>
2024-07-21 22:40:47 +0530

    Optimized Frontend Using Lazy Loader

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Components
-import Login from "./components/Login";
-import Forgot from "./components/Forgot";
-import Register from "./components/Register";
-import Otp from "./components/Otp";
-import Dashboard from "./components/Dashboard";
-import Changepass from "./components/Changepass";
-import Search from "./components/Dashboard Components/Search";
-import Notification from "./components/Dashboard Components/Notification";
-import Create from "./components/Dashboard Components/Create";
-import Message from "./components/Dashboard Components/Message";
-import Profile from "./components/Dashboard Components/Profile";
-import User from "./components/Dashboard Components/User";
-import Editprofile from "./components/Dashboard Components/Editprofile";
-import Shareprofile from "./components/Dashboard Components/Shareprofile";
-import Post from "./components/Dashboard Components/Post";
-import Pnf from "./components/Pnf";
-
-// Common Components
-import Leftdashboard from "./components/Dashboard Components/Leftdashboard";
-import Dashboardnav from "./components/Dashboard Components/Dashboardnav";
-import Componentloader from "./components/Dashboard Components/Componentloader";
-import Userloader from "./components/Dashboard Components/Userloader";
 
 // Stylesheets
 import "./styles/index.scss";
+
+// Components
+const Login = lazy(() => import("./components/Login"));
+const Forgot = lazy(() => import("./components/Forgot"));
+const Register = lazy(() => import("./components/Register"));
+const Otp = lazy(() => import("./components/Otp"));
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Changepass = lazy(() => import("./components/Changepass"));
+const Search = lazy(() => import("./components/Dashboard Components/Search"));
+const Notification = lazy(() =>
+  import("./components/Dashboard Components/Notification")
+);
+const Create = lazy(() => import("./components/Dashboard Components/Create"));
+const Message = lazy(() => import("./components/Dashboard Components/Message"));
+const Profile = lazy(() => import("./components/Dashboard Components/Profile"));
+const User = lazy(() => import("./components/Dashboard Components/User"));
+const Editprofile = lazy(() =>
+  import("./components/Dashboard Components/Editprofile")
+);
+const Shareprofile = lazy(() =>
+  import("./components/Dashboard Components/Shareprofile")
+);
+const Post = lazy(() => import("./components/Dashboard Components/Post"));
+const Pnf = lazy(() => import("./components/Pnf"));
+
+// Common Components
+const Leftdashboard = lazy(() =>
+  import("./components/Dashboard Components/Leftdashboard")
+);
+const Dashboardnav = lazy(() =>
+  import("./components/Dashboard Components/Dashboardnav")
+);
+const Componentloader = lazy(() =>
+  import("./components/Dashboard Components/Componentloader")
+);
+const Userloader = lazy(() =>
+  import("./components/Dashboard Components/Userloader")
+);
 
 function App() {
   useEffect(() => {
@@ -44,121 +58,123 @@ function App() {
     });
   });
   return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={Login} />
-        <Route path="/register" Component={Register} />
-        <Route path="/otp/:usertoken" Component={Otp} />
-        <Route path="/forgot-password" Component={Forgot} />
-        <Route
-          path="/changepass/:forgottoken/:usertoken"
-          Component={Changepass}
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <Dashboard
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-              Userloader={Userloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/search"
-          element={
-            <Search
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-              Userloader={Userloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/create"
-          element={
-            <Create
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/notification"
-          element={
-            <Notification
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/message"
-          element={
-            <Message
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/profile"
-          element={
-            <Profile
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/editprofile"
-          element={
-            <Editprofile
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/shareprofile"
-          element={
-            <Shareprofile
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/search/:username"
-          element={
-            <User
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route
-          path="/dashboard/post/:postusertoken/:posttoken"
-          element={
-            <Post
-              Leftdashboard={Leftdashboard}
-              Dashboardnav={Dashboardnav}
-              Componentloader={Componentloader}
-            />
-          }
-        />
-        <Route path="*" Component={Pnf} />
-      </Routes>
-    </Router>
+    <Suspense>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path="/otp/:usertoken" Component={Otp} />
+          <Route path="/forgot-password" Component={Forgot} />
+          <Route
+            path="/changepass/:forgottoken/:usertoken"
+            Component={Changepass}
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <Dashboard
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+                Userloader={Userloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/search"
+            element={
+              <Search
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+                Userloader={Userloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/create"
+            element={
+              <Create
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/notification"
+            element={
+              <Notification
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/message"
+            element={
+              <Message
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/profile"
+            element={
+              <Profile
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/editprofile"
+            element={
+              <Editprofile
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/shareprofile"
+            element={
+              <Shareprofile
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/search/:username"
+            element={
+              <User
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route
+            path="/dashboard/post/:postusertoken/:posttoken"
+            element={
+              <Post
+                Leftdashboard={Leftdashboard}
+                Dashboardnav={Dashboardnav}
+                Componentloader={Componentloader}
+              />
+            }
+          />
+          <Route path="*" Component={Pnf} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
